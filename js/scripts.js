f9adc838209855dc6de77235b73e08b05a388e86
gozdecamlioglu <gozdecamlioglu@gmail.com>
2023-10-16 10:03:25 -0400

    Edited Overall
    Access to the contact page, profile picture on the about page, and some details on the index page have been edited

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
-        x.className += ' responsive';
-    } else {
-        x.className = 'navtoggle';
+    if(window.innerWidth < 641) {
+        var x = document.getElementById('myNavtoggle');
+        if (x.className === 'navtoggle') {
+            x.className += ' responsive';
+        } else {
+            x.className = 'navtoggle';
+        }
     }
 }
